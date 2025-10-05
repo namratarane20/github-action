be31f2a124a16f72788dbfbc6b3ca34cab84789c
Namrata Rane (TMNA) <namrata.rane@toyota.com>
2025-10-05 06:45:02 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// https://github.com/namratarane20/github-action.git
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+// Simple GET route
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
+// GET route with params
+app.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello, ${name}!`);
+});
+
+// POST route with JSON body
+app.post('/add', (req, res) => {
+  const { a, b } = req.body;
+  const result = a + b;
+  res.json({ result });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`API server running at http://localhost:${port}`);
+});
